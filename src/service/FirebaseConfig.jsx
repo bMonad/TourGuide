d660dbec9069b968933f8a51746849b2f0825432
Shiva1706 <shivansh.singh20021@gmail.com>
2025-08-26 02:09:56 +0530

    Deployed!

--- src/service/FirebaseConfig.jsx
+++ src/service/FirebaseConfig.jsx
@@ -1,21 +1,15 @@
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// import { getAnalytics } from "firebase/analytics";
-
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-    apiKey: "AIzaSyByuld-FnN-w6s2mNbpAIHO5TbVSrGOvh0",
-    authDomain: "tourguide-8c872.firebaseapp.com",
-    projectId: "tourguide-8c872",
-    storageBucket: "tourguide-8c872.firebasestorage.app",
-    messagingSenderId: "497052320116",
-    appId: "1:497052320116:web:290ac192d8e05b9933ac21",
-    measurementId: "G-B02MHHVSRF"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
-// const analytics = getAnalytics(app);
